632ecc1ab11ef797e3bfa8fe5401b175d7751f4b
Michael Stokoe <mstokoe0990@gmail.com>
2023-09-22 19:43:00 +0100

    Added a back button

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="container flex flex-col mx-auto space-y-6">
-        <div class="py-6 text-center">
-            <h1 class="text-5xl font-semibold text-white">{{ gameTitle }}</h1>
+        <div class="grid w-full grid-cols-3 py-6">
+            <div class="col-span-1 text-left">
+                <router-link to="/" class="text-2xl font-semibold text-white hover:underline">
+                    Go Back</router-link>
+            </div>
+            <div class="col-span-1 text-center">
+                <h1 class="text-5xl font-semibold text-white">{{ gameTitle }}</h1>
+            </div>
+            <div class="col-span-1"></div>
         </div>
 
         <div class="flex flex-col p-6 space-y-4 text-white bg-gray-800 rounded-lg">
